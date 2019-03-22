499c994a7ea6e943f70dcb4f19e46a36b924244c
fredemoes <fm@tyfc.co>
2019-03-22 13:57:32 +0100

    Frederik upload

---
+++ javascript_quiz.js
@@ -0,0 +1,9 @@
+class Question {
+    constructor(question, answer) {
+    this.question = question;
+    this.answer = answer;
+    }
+}
+
+const question1 = new Question('What is my name?', 'Frederik');
+
