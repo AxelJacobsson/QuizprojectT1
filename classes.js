5b28c377240b5a292009b290851c874c813c18fa
Axel Jacobsson <axel-jacobsson@outlook.com>
2019-03-22 14:20:57 +0100

    Axel clean up

---
+++ classes.js
@@ -0,0 +1,28 @@
+//CLASSES
+
+//#1 Class: User 
+class User {
+    constructor(name) {
+        this.name = name;
+    }
+};
+
+
+//#2 Class: Question generator
+class QuestionGen {
+    constructor(category, question, wrong1, wrong2, correct) {
+        this.category = category;
+        this.question = question;
+        this.wrong1 = wrong1;
+        this.wrong2 = wrong2;
+        this.correct = correct;
+    }
+};
+
+//#3 Class: Quiz generator
+class QuizGen {
+    constructor() {
+        this
+    }
+};
+
