9c6ae82ab681c120d44998959c6e16151aca5fdc
fredemoes <fm@tyfc.co>
2019-03-29 10:50:03 +0100

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,37 @@
+//Array with all questions
+
+const allQuestions = [
+    // Category: Conditionals
+    new QuestionGen('Conditionals', 'If isHungry equals true, which of the following expressions evaluates to true?', ['isHungry !== false', '!isHungry === true', 'isHungry === false'], 'isHungry !== false'),
+    new QuestionGen('Conditionals', 'What is the general purpose of a conditional statement?', ['Conditional statements answer binary (yes-or-no) questions.', 'Conditional statements evaluate code as either true or false.', 'Conditional statements make all computers capable of thought.'], 'Conditional statements evaluate code as either true or false.'),
+    new QuestionGen('Conditionals', 'Which of the following variables contains a truthy value?', ['let varThree = 0;', 'let varOne = "false";', 'let varFour = "";'], 'let varOne = "false";'),
+    // Category: Functions
+    new QuestionGen('Functions', 'questions', ['option1', 'option2', 'option3'], 'answer'),
+    new QuestionGen('Functions', 'questions', ['option1', 'option2', 'option3'], 'answer'),
+    new QuestionGen('Functions', 'questions', ['option1', 'option2', 'option3'], 'answer'),
+    // Category: Scope
+    new QuestionGen('Scope', 'questions', ['option1', 'option2', 'option3'], 'answer'),
+    new QuestionGen('Scope', 'questions', ['option1', 'option2', 'option3'], 'answer'),
+    new QuestionGen('Scope', 'questions', ['option1', 'option2', 'option3'], 'answer'),
+    // Category: Arrays
+    new QuestionGen('Arrays', 'questions', ['option1', 'option2', 'option3'], 'answer'),
+    new QuestionGen('Arrays', 'questions', ['option1', 'option2', 'option3'], 'answer'),
+    new QuestionGen('Arrays', 'questions', ['option1', 'option2', 'option3'], 'answer'),
+    // Category: Loops
+    new QuestionGen('Loops', 'questions', ['option1', 'option2', 'option3'], 'answer'),
+    new QuestionGen('Loops', 'questions', ['option1', 'option2', 'option3'], 'answer'),
+    new QuestionGen('Loops', 'questions', ['option1', 'option2', 'option3'], 'answer'),
+    // Category: Iterators
+    new QuestionGen('Iterators', 'questions', ['option1', 'option2', 'option3'], 'answer'),
+    new QuestionGen('Iterators', 'questions', ['option1', 'option2', 'option3'], 'answer'),
+    new QuestionGen('Iterators', 'questions', ['option1', 'option2', 'option3'], 'answer'),
+    // Category: Objects
+    new QuestionGen('Objects', 'questions', ['option1', 'option2', 'option3'], 'answer'),
+    new QuestionGen('Objects', 'questions', ['option1', 'option2', 'option3'], 'answer'),
+    new QuestionGen('Objects', 'questions', ['option1', 'option2', 'option3'], 'answer'),
+    // Category: Classes
+    new QuestionGen('Classes', 'questions', ['option1', 'option2', 'option3'], 'answer'),
+    new QuestionGen('Classes', 'questions', ['option1', 'option2', 'option3'], 'answer'),
+    new QuestionGen('Classes', 'questions', ['option1', 'option2', 'option3'], 'answer')
+];
+
